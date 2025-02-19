7a71bd38eb9d18982f2b81db6e13629fab1aeed9
Jack Lukic <jacklukic@gmail.com>
2025-02-19 12:23:12 -0500

    Test another person typedef

--- types/person.d.ts
+++ types/person.d.ts
@@ -1,15 +1,16 @@
 /**
- * An interface representing a person.
- */
-export type Person {
+* A class representing a Person
+*/
+export class Person {
   name: string;
   age: number;
   readonly address?: string; // Optional and readonly
   hobbies: string[];
+  constructor(name: string, age: number, address?: string, hobbies?: string[]);
 }
 
 /**
- * A function that takes a Person object and logs their details
- * @param person The Person to introduce
- */
+* A function that takes a Person object and logs their details
+* @param person The Person to introduce
+*/
 export function introduce(person: Person): void;
